3c1f0713c4d8f82e1249143424f7b7e4b852c59e
Eric Whitcomb <eric.whitcomb@gmail.com>
2021-08-17 13:39:13 -0600

    Add hook for styles and fonts
    Add normalize css
    Add google fonts
    Add theme style

--- functions.php
+++ functions.php
@@ -10,4 +10,36 @@ function wp_gym_theme_menus() {
 
 // Hook
 add_action('init', 'wp_gym_theme_menus');
+
+// Adds Stylesheets and JS Files
+function wp_gym_theme_scripts() {
+
+  // Normalize CSS
+  wp_enqueue_style(
+    'normalize_css',
+    get_template_directory_uri() . "/css/normalize.css",
+    array(),
+    '8.0.1' 
+  );
+
+  // Google Fonts
+  wp_enqueue_style(
+    'google_fonts',
+    'https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&family=Raleway:wght@400;700;900&family=Staatliches&display=swap',
+    array(),
+    null
+  );
+
+  // Main Stylesheet
+  wp_enqueue_style(
+    'theme_style',
+    get_stylesheet_uri(),
+    array('normalize_css', 'google_fonts'),
+    '1.0.0'
+  );
+
+}
+
+add_action('wp_enqueue_scripts', 'wp_gym_theme_scripts');
+
 ?>
