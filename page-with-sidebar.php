cf06668adc4ada4d0d2242120aaf43349b1c48d1
Eric Whitcomb <eric.whitcomb@gmail.com>
2021-08-17 19:30:50 -0600

    Add and style page with sidebar template

---
+++ page-with-sidebar.php
@@ -0,0 +1,32 @@
+<?php
+/*
+ * Template Name: Page with Sidebar
+ */
+?>
+
+<?php get_header(); ?>
+
+<main class="container page section with-sidebar">
+  <div class="page-content">
+    <?php while(have_posts()): the_post(); ?>
+
+      <h1 class="text-center text-primary"><?php the_title(); ?></h1>
+      
+      <?php 
+        // check if a thumbnail exists
+        if(has_post_thumbnail()):
+          the_post_thumbnail('blog', array('class' => 'featured-image'));
+        endif;
+      ?>
+
+      <?php the_content(); ?>
+      
+    <?php endwhile; ?>
+  </div>
+
+  <aside class="sidebar">
+    <h2>Sidebar Here</h2>
+  </aside>
+</main>
+
+<?php get_footer(); ?>
--- page.php
+++ page.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 
-<main class="container page section no-sidebars">
+<main class="container page section no-sidebar">
   <?php while(have_posts()): the_post(); ?>
 
     <h1 class="text-center text-primary"><?php the_title(); ?></h1>
