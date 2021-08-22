4b54277842f101fe456f26d7e9ff091d6cfdbdab
Eric Whitcomb <eric.whitcomb@gmail.com>
2021-08-22 15:48:32 -0600

    Display images in gallery using custom sizes

--- gallery.php
+++ gallery.php
@@ -10,8 +10,26 @@
   <?php while(have_posts()): the_post(); ?>
   
     <h1 class="text-center text-primary"><?php the_title(); ?></h1>
-    <?php the_content(); ?>
-  
+    
+    <?php 
+      $gallery = get_post_gallery(get_the_ID(), false);
+
+      $image_ids = explode(",", $gallery['ids']);
+    ?>
+
+    <ul class="gallery-images">
+      <?php 
+        $i = 0;
+        foreach ($image_ids as $id):
+          $size = ($i === 3) || ($i === 6) ? 'portrait' : 'square';
+          $image = wp_get_attachment_image_src($id, $size);
+      ?>
+
+      <img src="<?php echo $image[0]; ?>" />
+
+      <?php $i++; endforeach; ?>
+    </ul>
+
   <?php endwhile; ?>
 </main>
 
