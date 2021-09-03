ba41e5c1c6a292c6cd60b8f5b24f26ca90aedaac
Eric Whitcomb <eric.whitcomb@gmail.com>
2021-09-02 23:53:29 -0600

    Add areas section to front page

--- front-page.php
+++ front-page.php
@@ -11,5 +11,32 @@
     <p><?php echo $welcome_text; ?></p>
   </section>
 
+  <!-- Areas Section -->
+  <?php
+    // This may seem like overkill, but this allows for new areas
+    // to be added and only the css would need to be updated.
+    $areas = Array();
+    $i = 1;
+    echo get_field('area_5');
+    while (true) {
+      $area = get_field('area_' . $i);
+      if ($area === null) break;
+      array_push($areas, $area);
+      $i++;
+    }
+  ?>
+  <section class="areas">
+    <ul class="areas-list">
+      <?php foreach ($areas as $area): ?>
+      <li class="area-item">
+        <img src="<?php echo wp_get_attachment_image_src(
+              $area['area_image'], 
+              'medium_size')[0]; ?>">
+        <p><?php echo $area['area_name']; ?>
+      </li>
+      <?php endforeach; ?>
+    </ul>
+  </section>
+
   <?php endwhile; ?>
 <?php get_footer(); ?>
