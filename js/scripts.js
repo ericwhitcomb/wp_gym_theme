a9c1300d93d629bc53d28a95ce4db0ff238968ef
Eric Whitcomb <eric.whitcomb@gmail.com>
2021-09-05 19:46:35 -0600

    Add fixed menu when scrolling
    Also add orange underline to footer menu items when current page

--- js/scripts.js
+++ js/scripts.js
@@ -9,4 +9,29 @@ jQuery(document).ready(($) => {
     controls: false,
     mode: "fade",
   });
+
+  // When the page is ready add the fixed menu if position is > 300px
+  const headerScroll = document.querySelector(".navigation-bar");
+  const rect = headerScroll.getBoundingClientRect();
+  const topDistance = Math(abs(rect.top));
+
+  fixedMenu(topDistance);
 });
+
+// Adds a fixed menu on top
+const fixedMenu = (scroll) => {
+  const headerScroll = document.querySelector(".navigation-bar");
+
+  // in the case the scroll is greater than 300 add some classes
+  if (scroll > 300) {
+    headerScroll.classList.add("fixed-top");
+  } else {
+    headerScroll.classList.remove("fixed-top");
+  }
+};
+
+// keeps track of the vertical scroll and calls fixed menu when needed
+window.onscroll = () => {
+  const scroll = window.scrollY;
+  fixedMenu(scroll);
+};
